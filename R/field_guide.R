ed7d9b4e867dbd456a83be9a024d321ac427aec7
Zachary S.L Foster <zacharyfoster1989@gmail.com>
2017-10-16 10:51:54 -0700

    updates

--- R/field_guide.R
+++ R/field_guide.R
@@ -1,75 +1,115 @@
+#' Return species for a coord range
+#'
+#' Return species for a coordinate range according to GBIF.
+#'
+#' @param lat_range (\code{numeric} of length 2) The range of latitudes to search species occurance data for.
+#' @param long_range (\code{numeric} of length 2) The range of longitudes to search species occurance data for.
+#' @param taxon (\code{character} of length 1) A taxon name to search for.
+#'
+#' @return Path to the output file
+#'
+#' @export
+search_area <- function(lat_range,
+                        long_range,
+                        taxon = NULL) {
+  # Search for species in range
+  gbif_occ <- rgbif::occ_search(decimalLatitude = lat_range,
+                                decimalLongitude = long_range,
+                                hasCoordinate = TRUE,
+                                return = "data",
+                                scientificName = taxon,
+                                limit = 10000)
+
+  # Filter out unneed columns
+  gbif_occ <- gbif_occ[, c("name", "scientificName", "kingdom", "phylum", "order", "family", "genus", "species")]
+
+  # Make unique
+  gbif_occ <- unique(gbif_occ)
+
+  # Convert to taxmap
+  output <- suppressWarnings(taxa::parse_tax_data(gbif_occ,
+                                                  class_cols = c("kingdom", "phylum", "order", "family", "genus", "species")))
+
+  return(output)
+}
 
 
-rgbif::occ_search(hasCoordinate = TRUE, decimalLatitude = "43,45", decimalLongitude = "-122,-120")
+#' Return species for a coord and radius
+#'
+#' Return species for a coordinate and radius according to GBIF.
+#'
+#' @param long_range (\code{numeric} of length 2) The range of longitudes to search species occurance data for.
+#' @param lat_range (\code{numeric} of length 2) The range of latitudes to search species occurance data for.
+#' @param radius (\code{numeric} of length 1) How far in kilometers from \code{place_name} to look for species.
+#' @param taxon (\code{character} of length 1) A taxon name to search for.
+#'
+#' @return Path to the output file
+#'
+#' @export
+search_radius <- function(lat, long, radius = 40, taxon = NULL) {
+  # Get range of coords to search
+  location <- as.numeric(c(lat, long))
+  lat_diff <- abs(radius / 110.574 / 2) # http://stackoverflow.com/questions/1253499/simple-calculations-for-working-with-lat-lon-km-distance
+  long_diff <- abs(radius / (111.32 * cos(location[2] * pi / 180)) / 2)
+  lat_range <- paste(location[1] - lat_diff, location[1] + lat_diff, sep = ",")
+  long_range <- paste(location[2] - long_diff, location[2] + long_diff, sep = ",")
 
+  # Search area
+  search_area(lat_range = lat_range, long_range = long_range, taxon = taxon)
+}
 
 
 #' Make a field guide for a location
 #'
 #' Uses a location name to make a pdf of wikipedia articles for all species found within a given radius according to GBIF.
 #'
-#' @param place (\code{character} of length 1) Where to search for species
-#' @param output_path (\code{character} of length 1) Path to the output file
-#' @param radius (\code{numeric} of length 1) How far in kilometers from \code{place} to look for species.
+#' @param place_name (\code{character} of length 1) Where to search for species.
+#' @param radius (\code{numeric} of length 1) How far in kilometers from \code{place_name} to look for species.
+#' @param taxon (\code{character} of length 1) A taxon name to search for.
+#' @param ask (\code{logical}) if \code{TRUE}, ask the user questions when there are ambiguities.
 #'
 #' @return Path to the output file
 #'
 #' @examples
 #' \dontrun{
-#' make_field_guide("prineville reservoir", "~/)
+#' search_place(place_name = "prineville reservoir")
 #' }
-make_field_guide <- function(place,
-                             output_path = paste0(gsub(" ", "_", place), ".pdf"),
-                             radius = 10) {
+#'
+#' @export
+search_place <- function(place_name,
+                         radius = 40,
+                         taxon = NULL,
+                         ask = FALSE) {
   # Get geographic coordinates for place name
-  possible_places <- geonames::GNsearch(name = place)
+  possible_places <- geonames::GNsearch(name = place_name)
   if (nrow(possible_places) == 0) {
-    stop(paste0('Could not find any matches to place name "', place,'"'))
+    stop(paste0('Could not find any matches to place name "', place_name,'"'))
   }
 
   # Ask user to choose if there are multiple hits
   if (nrow(possible_places) > 1) {
     possiblities <- apply(possible_places, MARGIN = 1,
                           function(x) paste(x['name'], x['adminName1'], x['countryName'], sep = ", "))
-    cat(paste0('Multiple possiblities found for "', place, '":\n'))
-    cat(paste(seq_along(possiblities), possiblities, collapse = "\n", sep = ":"))
-    choice = -1
-    while(choice < 1 ){
-      choice <- readline("Which location do you want?: ")
-      choice <- ifelse(grepl("\\D",choice),-1,as.integer(choice))
-      if(is.na(choice)){break}  # breaks when hit enter
+    if (ask) {
+      my_print('Multiple possiblities found for "', place, '":\n')
+      my_print(paste(seq_along(possiblities), possiblities, collapse = "\n", sep = ":"))
+      choice = -1
+      while(choice < 1 ){
+        choice <- readline("Which location do you want?: ")
+        choice <- ifelse(grepl("\\D",choice),-1,as.integer(choice))
+        if(is.na(choice)){break}  # breaks when hit enter
+      }
+    } else {
+      choice <- 1
+      my_print('Found "', possiblities[choice], '".')
     }
-    location <- as.numeric(unlist(possible_places[choice, c('lat', 'lng')]))
   } else {
-    location <- as.numeric(unlist(possible_places[1, c('lat', 'lng')]))
+    choice <- 1
   }
 
-  # Get range of coords to search in
-  lat_diff <- abs(radius / 110.574 / 2) # http://stackoverflow.com/questions/1253499/simple-calculations-for-working-with-lat-lon-km-distance
-  long_diff <- abs(radius / (111.32 * cos(location[2] * pi / 180)) / 2)
-  lat_range <- paste(location[1] - lat_diff, location[1] + lat_diff, sep = ",")
-  long_range <- paste(location[2] - long_diff, location[2] + long_diff, sep = ",")
+  # Get location coords
+  location <- as.numeric(unlist(possible_places[choice, c('lat', 'lng')]))
 
-  # Search for species in range
-  gbif_occ <- rgbif::occ_search(decimalLatitude = lat_range,
-                                decimalLongitude = long_range,
-                                hasCoordinate = TRUE,
-                                return = "data",
-                                limit = 10000)
-  species_in_range <- unique(gbif_occ$species)
-  species_in_range <- species_in_range[!is.na(species_in_range)]
-
-  # Check that pages exist on wikipedia
-  is_on_wiki <- function(a_name) {
-    info <- WikipediR::page_info("en", "wikipedia", page = a_name)
-    names(info$query$pages)[1] != -1
-    Sys.sleep(0.5) # be nice to wiki servers
-  }
-  on_wiki <- vapply(species_in_range, is_on_wiki, logical(1))
-  species_in_range <- species_in_range[on_wiki]
-
-  # Make PDF of wiki articles
-  wiki_urls <- paste0("https://en.wikipedia.org/wiki/", gsub(" ", "_", species_in_range))
-  command <- paste("wkhtmltopdf", "--zoom", 0.5, paste(wiki_urls, collapse = " "), output_path)
-  system(command)
+  # Search place
+  search_radius(lat = location[1], long = location[2], radius = radius, taxon = taxon)
 }
