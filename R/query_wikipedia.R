b4a4b28e0c65c4811aa08848ed51117fa6323ac7
Zachary S.L Foster <zacharyfoster1989@gmail.com>
2017-12-01 15:48:30 -0800

    wikipedia querying redone

--- R/query_wikipedia.R
+++ R/query_wikipedia.R
@@ -8,6 +8,7 @@
 #'
 #' @export
 query_wikipedia <- function(obj) {
+
   # Get list of species to look up
   species <- unique(obj$data$occ$name)
   names(species) <- obj$data$occ$taxon_id[match(species, obj$data$occ$name)]
@@ -17,6 +18,7 @@ query_wikipedia <- function(obj) {
   progress_bar <- txtProgressBar(min = 0, max = length(species), style = 3)
   content <- vapply(seq_len(length(species)), FUN.VALUE = character(1), function(i) {
     result <- get_wiki_content(species[i])
+    Sys.sleep(.3) # Be nice to Wikipedia's servers
     setTxtProgressBar(progress_bar, i)
     return(result)
   })
@@ -39,20 +41,20 @@ query_wikipedia <- function(obj) {
   wiki <- dplyr::tibble(taxon_id = rep(names(species), vapply(sections, length, numeric(1))),
                         query = rep(species, vapply(sections, length, numeric(1))),
                         title = unlist(titles),
-                        raw = unlist(sections))
-
-  # Record titles level
-  wiki$title_level <- nchar(stringr::str_match(wiki$title, pattern = "(=+).*")[, 2])
+                        title_level = nchar(stringr::str_match(unlist(titles), pattern = "(=+).*")[, 2]),
+                        content = unlist(sections))
 
   # Remove markup from titles
   wiki$title <- stringr::str_match(wiki$title, pattern = "=+ *([ a-zA-Z0-9]+) *=+")[, 2]
   wiki$title <- trimws(wiki$title)
 
-  # Add rmarkdown conversion
-  wiki$rmd <- vapply(wiki$raw, wiki_to_markdown, character(1))
-
   # Print results
   my_print("  Found Wikipedia content for ", length(unique(wiki$query)), " species.\n")
+
+  # Add results to input object
+  obj$data$wiki <- wiki
+
+  return(obj)
 }
 
 
@@ -66,29 +68,24 @@ query_wikipedia <- function(obj) {
 #'
 #' @keywords internal
 get_wiki_content <- function(taxon) {
-  raw_result <-  RCurl::getForm(
-    "https://en.wikipedia.org/w/api.php",
-    action  = "query",
-    titles = taxon,
-    prop = "revisions",
-    rvprop = "content",
-    format = "json",
-    formatversion = "2"
-  )
+  # Query wikipedia
+  base_url <- "https://en.wikipedia.org/w/api.php"
+  raw_result <- RCurl::getURL(paste0(base_url,
+                                     "?format=json&action=query&prop=extracts&exlimit=max&explaintext&titles=",
+                                     gsub(taxon, pattern = " ", replacement = "_")))
 
   # Parse result
   parsed_result <- RJSONIO::fromJSON(raw_result)
 
   # Get content of article
-  content <- unname(parsed_result$query$pages[[1]]$revisions[[1]]["content"])
+  content <- unname(parsed_result$query$pages[[1]]$extract)
 
   # Handle redirects
-  if (! is.null(content) && grepl(content, pattern = "^#REDIRECT \\[\\[.+\\]\\].*$")) {
-    redirect <- gsub(content, pattern = "^#REDIRECT \\[\\[(.+?)\\]\\].*$", replacement = "\\1")
-    return(get_wiki_content(redirect))
-  }
+  # if (! is.null(content) && grepl(content, pattern = "^#REDIRECT \\[\\[.+\\]\\].*$")) {
+  #   redirect <- gsub(content, pattern = "^#REDIRECT \\[\\[(.+?)\\]\\].*$", replacement = "\\1")
+  #   return(get_wiki_content(redirect))
+  # }
 
-  # Convert to markdown
   if (is.null(content)) {
     return("")
   } else {
