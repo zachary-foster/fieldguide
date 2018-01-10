65a66a1d499a12ddb0906ae0b95a3cdc65052b9b
Zachary S.L Foster <zacharyfoster1989@gmail.com>
2018-01-09 22:30:45 -0800

    got pdf building working

--- R/make_guide_pdf.R
+++ R/make_guide_pdf.R
@@ -29,6 +29,7 @@
 #'   default is a temporary file.
 #' @param source_dir Where the save the source. The default is a temporary
 #'   directory.
+#' @param open If \code{TRUE}, open the PDF after it is made. (experimental)
 #'
 #' @inheritParams simple_pdf_cover
 #'
@@ -46,36 +47,36 @@ make_guide_pdf <- function(obj,
                            sp_photo = "inat" %in% names(obj$data),
                            sp_photo_max = 6,
                            sp_wiki = "wiki" %in% names(obj$data),
-                           out_path = NULL,
-                           source_dir = NULL,
-                           overwrite = FALSE) {
+                           out_name = NULL,
+                           out_dir = NULL,
+                           open = FALSE) {
   content <- character(0)
   sp_content <- list()
 
+  # Parse output name options
+  if (is.null(out_name)) {
+    out_name <- text_to_file_name(title)
+  }
+
   # Parse source directory option
-  if (is.null(source_dir)) {
-    source_dir <- tempfile(pattern = "fieldguide")
-    dir.create(source_dir)
-  } else if (dir.exists(source_dir)) {
+  if (is.null(out_dir)) {
+    out_dir <- tempfile(pattern = out_name)
+  }
+  if (! dir.exists(out_dir)) {
+    dir.create(out_dir)
+  }
+
+  # Check that the output will not overwrite anything
+  output_pdf_path <- file.path(out_dir, paste0(out_name, ".pdf"))
+  if (file.exists(output_pdf_path)) {
     if (overwrite) {
-      message(paste0('Replacing existing source directory at "', source_dir, '"'))
-      unlink(source_dir, recursive = TRUE)
+      message(paste0('Replacing existing output at "', output_pdf_path, '"'))
     } else {
-      stop(paste0('Source directory "', source_dir, '" already exists.\n',
-                  '  Set "overwrite" to TRUE to replace the directory.'))
+      stop(paste0('File "', source_dir, '" already exists.\n',
+                  '  Set "overwrite" to TRUE to replace the file'))
     }
   }
 
-  # Parse output path options
-  if (is.null(out_path)) {
-    out_path <- source_dir
-  }
-  if (dir.exists(out_path)) {
-    out_path <- file.path(out_path, text_to_file_name(title))
-  }
-  if (! grepl(out_path, pattern = "\\.pdf$", ignore.case = TRUE)) {
-    out_path <- paste0(out_path, ".pdf")
-  }
 
   # Make cover
   if (! (is.null(cover) | is.na(cover))) {
@@ -103,14 +104,40 @@ make_guide_pdf <- function(obj,
 
 
   # Make guide source
-  raw_source <- paste0(cover, collapse = "\n")
-  source_path <- file.path(source_dir, "fieldguide_source.Rnw")
+  header <- paste(sep = "\n",
+                  "\\documentclass{article}",
+                  "",
+                  "\\begin{document}",
+                  "")
+  footer <-  paste(sep = "\n",
+                   "",
+                   "\\end{document}")
+  raw_source <- paste(sep = "\n",
+                      header,
+                      paste0(content, collapse = "\n"),
+                      footer)
+  source_name <- paste0(out_name, ".Rnw")
+  source_path <- file.path(out_dir, source_name)
+
+  # Write guide source
   writeChar(object = raw_source,  con = source_path, eos = NULL)
 
   # Build guide
-  knitr::knit2pdf(source_path, output = out_path)
+  old_wd <- getwd()
+  setwd(out_dir)
+  on.exit(setwd(old_wd))
+  knitr::knit2pdf(source_name)
 
-  return(out_path)
+  # Open guide
+  if (open) {
+    system2("see", output_pdf_path, stdout = FALSE, stderr = FALSE)
+    system2("open", output_pdf_path, stdout = FALSE, stderr = FALSE)
+  }
+
+  # Print output file
+  message(paste0("Output file created at:\n  ", output_pdf_path))
+
+  return(output_pdf_path)
 }
 
 
@@ -134,8 +161,15 @@ make_guide_pdf <- function(obj,
 simple_pdf_cover <- function(title, cover) {
   cover <- tolower(cover)
   if (cover == "basic") {
-
+    content <- paste(sep = "\n",
+                     "\\topskip0pt",
+                     "\\vspace*{\\fill}",
+                     title,
+                     "\\vspace*{\\fill}",
+                     "%")
   } else {
     stop(call. = FALSE, paste0('Unknown cover style "', cover, '".'))
   }
+
+  return(content)
 }
